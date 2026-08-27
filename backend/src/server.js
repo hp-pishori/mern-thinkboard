637f8dfdd82e709c7b09cd1aba0b2a6462961c0d
Huzii <huziii@Huziis-MacBook-Air.local>
2026-08-27 12:20:56 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,21 +3,31 @@ import noteRoutes from "./routes/noteRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 // middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+}
 app.use(express.json());
 
 app.use("/api/notes", noteRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 connectDB();
 app.listen(PORT, () => {
   console.log("server started on port:", PORT);
